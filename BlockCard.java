7380a655ae5bab5af4626a998638f41c142b022d
xusandrew <65879913+xusandrew@users.noreply.github.com>
2022-06-16 09:38:47 -0400

    Update BlockCard.java

--- BlockCard.java
+++ BlockCard.java
@@ -1,46 +1,93 @@
+import javax.swing.*;
+
 public class BlockCard {
     private String color;
     private String other;
+    private ImageIcon img;
 
-    public void BlockCard(){
+    public BlockCard(){
         color = "";
         other = "";
+        img = findImage();
     }
 
-    public void BlockCard(String c){
+
+    public BlockCard(String c){
         color = c;
-
-        String[] list1 = {"yellow","orange","purple","grey"};
-        String[] list2 = {"black","red","blue","green"};
-
-        int index = 0;
-        while (other.equals("")){
-            String color1 = list1[index];
-            String color2 = list2[index];
-
-            if (color1.equals(color))
-                other = color2;
-            else if (color2.equals(color))
-                other = color1;
-        }
+        other = findOther();
+        img = findImage();
     }
 
     public String getColor(){
         return color;
     }
 
+    public String getOther(){
+        return other;
+    }
+    
+    public ImageIcon getImage(){
+        return img;
+    }
+
     public void setColor(String c){
         color = c;
+        other = findOther();
+    }
+
+    public void setOther(String o){
+        other = o;
+    }
+
+    public void setImage(ImageIcon i){
+        img = i;
     }
 
     public String toString(){
         return "The card color is "+ color;
     }
 
-    public void switch_color(){
+    public void switchColor(){
         String temp = color;
         color = other;
         other = temp;
+
+        img = findImage();
     }
+
+    public String findOther(){
+        String[] list1 = {"yellow","orange","purple","grey"};
+        String[] list2 = {"black","red","blue","green"};
+
+        int index = 0;
+        String ans = "";
+        while (ans.equals("")){
+            String color1 = list1[index];
+            String color2 = list2[index];
+
+            if (color1.equals(color))
+                ans = color2;
+            else if (color2.equals(color))
+                ans = color1;
+
+            index++;
+        }
+
+        return ans;
+    }
+
+    public ImageIcon findImage(){
+        return createImageIcon("pics/" + color + ".jpg");
+    }
+
+    protected static ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = XuShiftingStones.class.getResource(path);
+		if (imgURL != null)
+			return new ImageIcon(imgURL);
+		else
+			return null;
+	}
+
+    
 
 }
